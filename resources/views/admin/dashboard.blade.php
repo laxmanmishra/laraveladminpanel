94c27b6b9f222cd4183d038d6140b91058cc338a
laxmanmishra <laxmanmishrabbk@gmail.com>
2018-12-30 03:22:49 +0530

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.layouts.master')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Check</div>
+
+                <div class="card-body">
+                    Hello World
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
